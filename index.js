1bc4156bd894bd9f1a616bd7c082e38bfd05ca7d
Daniel Becker <daniel.becker000@gmail.com>
2022-04-04 21:09:41 -0400

    commit tree d3fcd0af577cf7d10803afcbc856f6d94e31a08a

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+ if (ride <=400) {
+   return "This one is on me!"
+ }
+ if (ride >= 400 && ride <= 2000) {
+  return "That will be twenty bucks."
+}
+if (ride >2000 && ride <=2500) {
+  return "I will gladly take your thirty bucks."
+}
+if (ride >2500) {
+  return "No can do."
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(ride){
+  
 }
 
 function switchOnCharmFromTip(){
